177ed265d204d73615661ea72c3f58346be078bf
Dember <salinas.dember@gmail.com>
2025-12-31 19:21:35 -0600

    Proyect start

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+import pygame
+
+def main():
+    #training()
+    #play()
+    print("2048 ended succefull")
+    
+
+if __name__ == "__main__":
+    main()
---
+++ settings.py
@@ -0,0 +1,12 @@
+# settings.py
+
+FPS=60
+
+# Game Size
+
+
+# Screen Distribution
+CELL_LENGTH=50
+BORDER_LENGTH=10
+EDGE_LENGTH=4
+WIDTH, HEIGHT=1080, 720
