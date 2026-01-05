4b097fc1f6e4b46bddfeaa09c7282ef530ac36fd
Dember <salinas.dember@gmail.com>
2026-01-04 18:20:23 -0600

    ytainning started

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pygame
 
 from game import Game
-from settings import WIDTH, HEIGHT, FPS
+from settings import WIDTH, HEIGHT, FPS, IA_TRAINING
 
 def play():
     pygame.init()
@@ -26,6 +26,30 @@ def play():
         pygame.display.flip()
         clock.tick(FPS)
     pygame.quit()
+
+def training():
+    for i in range(IA_TRAINING):
+        pygame.init()
+        pygame.font.init()
+        pygame.mixer.init()
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("2048")
+        clock = pygame.time.Clock()
+
+        game = Game(screen)
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            running = running and game.handle_input()
+            game.draw()
+
+            pygame.display.flip()
+            clock.tick(FPS)
+        pygame.quit()
 
 def main():
     #training()
